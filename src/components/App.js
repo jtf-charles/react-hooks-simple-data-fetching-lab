ad787dd9057f93b6be09a803ecd2a33ed1b32a11
unknown <jtf-charles>
2025-07-08 16:03:33 -0500

    Lab completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
+import { response } from "msw";
+import { useEffect,useState } from "react";
+
 // create your App component here
+
+function App(){
+const [image, setImage]=useState("")
+const [isloaded,setIsloaded]=useState(false)
+
+useEffect (()=>{
+fetch("https://dog.ceo/api/breeds/image/random")
+.then((res)=> res.json())
+.then ((data)=> {
+    setImage(data.message)
+    setIsloaded(true);
+}) 
+
+},[])
+
+if (!isloaded) return <p>Loading...</p>;
+return <img alt="A Random Dog" src={image}></img>;
+}
+
+export default App;
